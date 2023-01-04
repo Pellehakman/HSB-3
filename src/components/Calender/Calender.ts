e5bbdee3124ab57b2029fa4fbdd44ebeb066a648
Pellehakman <perhakman@outlook.com>
2023-01-04 15:24:33 +0100

    Update Calender.ts

--- src/components/Calender/Calender.ts
+++ src/components/Calender/Calender.ts
@@ -9,52 +9,42 @@ export default defineComponent({
      setup (){
         const db = getFirestore() 
 
-
-
-
-
    async function createDate() {
-            const daysMonth:any = [];
-            const todayDate = ref(new Date()).value;
-            const weekStartDate = startOfWeek(todayDate);
-            const daysInMonth = getDaysInMonth(todayDate);
-            
-            // for (let day = 0; day < daysInMonth; day++) {
-            //   let object = ref(format(addDays(weekStartDate, day), "E d MMMM")).value
-            //       console.log(object)
-            // }
-              
            
-            for (let day = 0; day < daysInMonth; day++) {
-              await setDoc(doc(db, "calender", `${day}`), {
-                    date: format(addDays(weekStartDate, day), "E d MMMM"),
-                    Id: Date.now(),
-                        
-                          slot1: {
-                            time: "07:00 till 11:00",
-                            id: null
-                          },
-                          slot2: {
-                            time: "11:00 till 15:00",
-                            id: null
-                          },
-                          slot3: {
-                            time: "15:00 till 19:00",
-                            id: null
-                          },
-                          slot4: {
-                            time: "19:00 till 23:00",
-                            id: null
-                          } 
-                        
-                      }
-                    )
-                  }
-                }
-        return {
-          createDate
+    const todayDate = ref(new Date()).value;
+    const daysInMonth = getDaysInMonth(todayDate);
+    const weekStartDate = startOfWeek(todayDate);
+  
+    for (let day = 0; day < daysInMonth; day++) {
+      let object = ref(format(addDays(weekStartDate, day), "E d MMMM")).value
+
+          await setDoc(doc(db, "calender", `${object}`), {
+
+            date: format(addDays(weekStartDate, day), "E d MMMM"),
+            Id: Date.now(),
+            slot1: {
+              time: "07:00 till 11:00",
+              id: null
+            },
+            slot2: {
+              time: "11:00 till 15:00",
+              id: null
+            },
+            slot3: {
+              time: "15:00 till 19:00",
+              id: null
+            },
+            slot4: {
+              time: "19:00 till 23:00",
+              id: null
+            }  
+          })      
         }
+      }
+    return {
+      createDate
     }
+}
    
 
 
