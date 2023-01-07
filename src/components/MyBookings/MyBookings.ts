73911849902f16f43faeae3e344eb5f38e32bf88
Pellehakman <perhakman@outlook.com>
2023-01-07 05:00:43 +0100

    1

--- src/components/MyBookings/MyBookings.ts
+++ src/components/MyBookings/MyBookings.ts
@@ -15,28 +15,13 @@ export default defineComponent({
         return data;
       });
 
-      let john = mybookingsDocs.filter(f => f.slot1.id === uid)
+      let findBookings = mybookingsDocs.filter(f => f.slot1.id === uid)
 
-  
-
-      let find = john.filter((f) => {
-        if (f.slot1.id === uid){
-          console.log(f)
-        }
-      })
-     
     
- 
 
 
-
-     
-      let da = john.filter((f) => {
-        if(f.slot1.id === uid){
-          return (f.slot1.map(b => b.id));
-        }
-      }); 
-      console.log(da)
+      console.log(findBookings)
+   
   
    
   },
